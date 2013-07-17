ac28e46114908b288a74904054e739d0831cdcfa
ti.shan@hotmail.com <ti.shan@hotmail.com>
2013-07-17 11:05:54 +0200

    simple dopaClient and Server RabbiMQ Version Hello World
    Change-Id: Id494cd447bfc4192e9bdfd4cdea7075ded429cfd

--- src/main/java/eu/stratosphere/meteor/client/DOPAClient.java
+++ src/main/java/eu/stratosphere/meteor/client/DOPAClient.java
@@ -1,5 +1,12 @@
 package eu.stratosphere.meteor.client;
 
+import java.io.IOException;
+
+import com.hp.hpl.jena.graph.Factory;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
 import eu.stratosphere.meteor.common.Submitable;
 
 /**
@@ -14,17 +21,46 @@ import eu.stratosphere.meteor.common.Submitable;
  *
  */
 public class DOPAClient implements Submitable {
-
+	private String QUEUE_NAME;
+  
+	private Connection connection;
+	private Channel statusQueue = null; 
+	
+    
 	@Override
 	public void createStatusQueue(String queueName) {
 		// TODO Auto-generated method stub
-
+		this.QUEUE_NAME=queueName;
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	  		
+	    try {
+			connection = factory.newConnection();
+		    statusQueue = connection.createChannel();
+		    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  
 	}
 
 	@Override
 	public void submit(String meteorScript) {
 		// TODO Auto-generated method stub
+		
+		 try {
+			 statusQueue.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    	 statusQueue.basicPublish("", QUEUE_NAME, null, meteorScript.getBytes());
+			 statusQueue.close();
+			 connection.close();
+		 } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		    System.out.println(" [x] Sent '" + meteorScript + "'");
+		       
 	}
 
 	@Override
@@ -36,9 +72,12 @@ public class DOPAClient implements Submitable {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)throws java.io.IOException {
 		// TODO Auto-generated method stub
-
+		 String message = "Hello du da!";
+		 DOPAClient firstCall=new DOPAClient();
+		 firstCall.createStatusQueue("Status");
+		 firstCall.submit(message);
 	}
 
 }
--- src/main/java/eu/stratosphere/meteor/server/DOPAScheduler.java
+++ src/main/java/eu/stratosphere/meteor/server/DOPAScheduler.java
@@ -1,5 +1,14 @@
 package eu.stratosphere.meteor.server;
 
+import java.io.IOException;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
 /**
  * 
  * Represents the server to submit jobs.
@@ -15,8 +24,49 @@ public class DOPAScheduler {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 
+	private final static String QUEUE_NAME = "hello from the server";
+	
+	public void run(){
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Channel channel = null;
+		QueueingConsumer consumer = null;
+		
+		try {
+			Connection connection = factory.newConnection();
+			channel = connection.createChannel();
+			
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			
+			consumer = new QueueingConsumer(channel);
+			channel.basicConsume(QUEUE_NAME, true, consumer);
+			
+		    while (true) {
+		      QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+		      String message = new String(delivery.getBody());
+		      System.out.println(" [x] Received '" + message + "'");
+		    }
+		    
+		} catch (IOException | ShutdownSignalException | ConsumerCancelledException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Waiting for messages.");
+		
+	}
+	
+	public static void main(String[] args) throws java.io.IOException,
+                   java.lang.InterruptedException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
 	}
 
